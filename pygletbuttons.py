99abcaeb2c6c04f0f2090cf3a89e3c8a71f3c33c
cwdi <cwalker@dimin.com>
2014-01-13 10:05:06 -0600

    Create pygletbuttons.py

---
+++ pygletbuttons.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# pyglet
+# Copyright (c) 2006-2008 Alex Holkner
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions 
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of pyglet nor the names of its
+#    contributors may be used to endorse or promote products
+#    derived from this software without specific prior written
+#    permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------------
+
+'''
+'''
+
+
+
+import pyglet
+from pyglet.window import key
+
+player = pyglet.media.Player()
+
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, world', 
+                          font_name='Times New Roman', 
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+def bpress(y):
+        music = pyglet.media.load(y)
+        player.play()
+def apress():
+    player.pause()
+def dpress(y):
+    player.pause()
+    music = pyglet.media.load(y)
+    player.queue(music)
+    player.play()
+
+
+def kinput(self, keypress):
+        print "Press a key (Escape key to exit):"
+        root.bind_all('<Key>', keypress)
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.B:
+        bpress('/Users/curtis/Audio/hey.wav')
+        print 'The "b" key was pressed.'
+    elif symbol == key.A:
+        apress()
+        print 'The a  key was pressed.'
+    elif symbol == key.D:
+        dpress('/Users/curtis/Audio/6days.mp3')
+        print 'The d key was pressed.'
+
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+pyglet.app.run()
